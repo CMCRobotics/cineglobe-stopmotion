7711a6eff57a2720424e3a10e3af89334b5af452
Brice Copy <brice.copy@users.noreply.github.com>
2019-06-29 11:20:26 +0200

    Add working CORS streaming, step control

---
+++ src/main/resources/static/app.js
@@ -0,0 +1,88 @@
+import {PoppyRobot} from './poppy/PoppyRobot.js';
+
+export const robot = new PoppyRobot("http://ergo-twelve.home:8080");
+
+//export const robot = new PoppyRobot("http://ergo-nine.cern.ch:8080");
+
+//export const robot = new PoppyRobot("http://localhost:8080");
+
+let stepCount = 0;
+let steps = 10;
+let tween = null;
+
+var startPosition = {m1:10, m2:-30, m3:-30, m4:50,m5:30};
+var position = Object.assign({}, startPosition);
+var endPosition = { m1: 10, m2:-18, m3:-40, m4:20,m5:50 };
+
+function stepPosition(){
+    tween.update(stepCount);
+    robot.set([robot.m1.goal_position,position.m1]
+             ,[robot.m2.goal_position,position.m2]
+             ,[robot.m3.goal_position,position.m3]
+             ,[robot.m4.goal_position,position.m4]
+             ,[robot.m5.goal_position,position.m5])
+             .then(function(response){
+                     console.log("Step "+stepCount);
+             });
+    if(stepCount < steps) stepCount++;
+}
+$("#step").on("click", function(){
+        stepPosition();
+});
+
+$("#reset").on("click", function(){
+        stepCount = 0;
+        stepPosition();
+});
+$("#undo").on("click", function(){
+        stepCount=stepCount-2;
+        if (stepCount < 0) stepCount = 0;
+        stepPosition();
+});
+
+$("#release").on("click", function(){
+        robot.set([robot.m1.compliant , true],
+                  [robot.m2.compliant , true],
+                  [robot.m3.compliant , true],
+                  [robot.m4.compliant , true],
+                  [robot.m5.compliant , true],
+                  [robot.m6.compliant , true]);
+  
+});
+
+robot.connect().then(function(){
+  console.log(robot);
+  
+  robot.set([robot.m1.compliant , false],
+            [robot.m2.compliant , false],
+            [robot.m3.compliant , false],
+            [robot.m4.compliant , false],
+            [robot.m5.compliant , false],
+            [robot.m6.compliant , false]);
+  
+  robot.m1.goal_position.set(-60).then(response => console.log("moving ") )
+  .then(function(){
+  
+   /*   const registers = [robot.m1.goal_position, robot.m1.compliant];
+      robot.get(...registers).then( function(response){
+          console.log(registers.map( r => r.currentValue ));
+     */
+    /*      
+          robot.set([robot.m1.goal_position,10],[robot.m2.goal_position,20])
+             .then(function(response){
+                     console.log("Completed");
+                     const motors = [robot.m1.goal_position, robot.m2.goal_position];
+                     robot.get(...motors).then( resp =>
+                         console.log(motors.map( r => r.currentValue )));
+             });
+             
+     */
+     // });
+  });
+  
+  tween = new TWEEN.Tween(position);
+  tween.to(endPosition, steps);
+  tween.start(0);
+  
+});
+
